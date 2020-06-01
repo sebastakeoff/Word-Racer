9f6d40512756afe44d34546ec115e4c0c0e56952
Francisco Tabares <42662080+fcotabar@users.noreply.github.com>
2020-05-31 20:37:41 -0500

    Create Main Class

---
+++ src/WordRacer.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Grupo 11 - Paradigmas de programación
+ */
+public class WordRacer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
