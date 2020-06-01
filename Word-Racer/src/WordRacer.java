1812b718eb2e54190c3233018d6686ade00bd6b3
fcotabar <fcotabar@gmail.com>
2020-05-31 19:15:03 -0500

    Main Class created

---
+++ Word-Racer/src/WordRacer.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author grupo11 - paradigmas de programación
+ */
+public class WordRacer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
